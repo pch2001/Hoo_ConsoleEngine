649048335b7c87ea20acce7c539a52cefcfbd5ee
pch2001 <65890886+pch2001@users.noreply.github.com>
2026-01-29 11:46:43 +0900

    Config 폴더에 엔진 설정 파일 추가 / 설정 값 읽기 기능 추가
    - Config/Setting.txt 파일에 엔진 설정 저장
    - 단순한 로드 기능 추가

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include "Engine/Engine.h"
 #include "Level/TestLevel.h"
-#include "Actor/TestActor.h"
 
 using namespace Wanted;
 
 int main()
 {
-	//Wanted::Engine engine;
-	//engine.SetNewLevel(new TestLevel());
-	//engine.Run();
+	Wanted::Engine engine;
+	engine.SetNewLevel(new TestLevel());
+	engine.Run();
 
+	/*
 	TestLevel* level = new TestLevel();
 	//Actor* actor = new Actor();
 
@@ -31,5 +31,5 @@ int main()
 	}
 	else {
 		std::cout << "actor is not TestActor type.\n";
-	}
+	}*/ // RTTI
 }
